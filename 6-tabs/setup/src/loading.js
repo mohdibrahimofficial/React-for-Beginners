d78913937ce5fa4784f63e0ab57af646f1d4e4da
mohdibrahimofficial <mohdibrahim972002@gmail.com>
2021-04-05 14:55:32 +0530

    Day 4 Tabs

---
+++ 6-tabs/setup/src/loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Loading()
+{
+    return (
+        <section className="section loading">
+            <h1>Loading....</h1>
+        </section>
+    )
+}
+
+export default Loading
