2cbbdc6c0af371b3e5a90048bb0e2a36f0123113
mohdibrahimofficial <mohdibrahim972002@gmail.com>
2021-04-06 09:54:29 +0530

    Day 5 Slider Setup

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -3,7 +3,40 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
-  return <h2>slider project setup</h2>;
+  const [people,setPeople] = useState(data);
+  const [index,setIndex] = useState(0);
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2><span>/</span>Reviews</h2>
+      </div>
+      <div className="section-center">
+        {
+          people.map((people,peopleIndex)=>{
+            const {id,image,name,title,quote} = people;
+            //More Stuff Coming
+
+            return (
+              <article key={id}>
+                  <img src={image} alt={name} className="person-img" />
+                  <h4>{name}</h4>
+                  <p className="title">{title}</p>
+                  <p className="text">{quote}</p>
+                  <FaQuoteRight className="icon" />
+              </article>
+            );
+          })
+        }
+        <button className="prev">
+          <FiChevronLeft />
+        </button>
+        <button className="next">
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
