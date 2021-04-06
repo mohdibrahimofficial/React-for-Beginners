87caf85732d4cb871c566dd604d1db8efdbe0e7d
mohdibrahimofficial <mohdibrahim972002@gmail.com>
2021-04-06 14:48:03 +0530

    Day 5 Reviews

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -6,6 +6,21 @@ function App() {
   const [people,setPeople] = useState(data);
   const [index,setIndex] = useState(0);
 
+  useEffect(()=>{
+    const lastIndex = people.length - 1;
+    if(index<0)
+      setIndex(lastIndex);
+    if(index===people.length)
+      setIndex(0);
+  },[index,people]);
+
+  useEffect(()=>{
+    let slider = setInterval(()=>{
+      setIndex(index+1);
+    },3000);
+    return (()=>clearInterval(slider));
+  },[index])
+
   return (
     <section className="section">
       <div className="title">
@@ -13,12 +28,20 @@ function App() {
       </div>
       <div className="section-center">
         {
-          people.map((people,peopleIndex)=>{
-            const {id,image,name,title,quote} = people;
+          people.map((person,personIndex)=>{
+            const {id,image,name,title,quote} = person;
             //More Stuff Coming
+            let position = 'nextSlide';
+            
+            if( index === personIndex )
+              position = 'activeSlide'
+
+            if( ( index-1 === personIndex ) ||
+                (index === 0 && personIndex === people.length-1) )
+              position = 'lastSlide'
 
             return (
-              <article key={id}>
+              <article key={id} className={position}>
                   <img src={image} alt={name} className="person-img" />
                   <h4>{name}</h4>
                   <p className="title">{title}</p>
@@ -28,10 +51,10 @@ function App() {
             );
           })
         }
-        <button className="prev">
+        <button className="prev" onClick={()=>setIndex(index-1)}>
           <FiChevronLeft />
         </button>
-        <button className="next">
+        <button className="next" onClick={()=>setIndex(index+1)}>
           <FiChevronRight />
         </button>
       </div>
