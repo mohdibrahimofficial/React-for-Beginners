9c9eac6142b654722a316fc1bd8b6256a69bc0ea
mohdibrahimofficial <mohdibrahim972002@gmail.com>
2021-04-08 07:29:10 +0530

    Day 7 Grocery Bud | Add items

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -3,7 +3,54 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name,setName] = useState('');
+  const [list,setList] = useState([]);
+  const [isEditing,setIsEditing] = useState(false);
+  const [editId,setEditId] = useState(null);
+  const [alert,setAlert] = useState({show:false,msg:'',type:''});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!name)
+    {
+      //display the alert
+    }
+    else if(name && isEditing)
+    {
+      //diff
+    }
+    else{
+      const newItem = {id:new Date().getTime().toString(),title:name};
+      setList([...list,newItem]);
+      setName('');
+    }
+  }
+
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show&&<Alert />}
+        <h3>Grocery Bud</h3>
+        <div className="form-control">
+          <input type="text" className="grocery" 
+          placeholder="e.g. eggs" 
+          value={name} 
+          onChange={(e)=>setName(e.target.value)}/>
+          <button type="submit" className="submit-btn">
+            {isEditing?'edit':'submit'}
+          </button>
+        </div>
+      </form>
+      { list.length > 0 && 
+          <div className="grocery-container">
+            <List items={list} />
+            <button className="clear-btn">
+              Clear Items
+            </button>
+          </div>
+      }
+    </section>
+  )
 }
 
 export default App
--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
+const List = ({items}) => {
+  return (
+    <div className="grocery-list">
+      {
+        items.map((item)=>{
+          const {id,title} = item;
+          console.log(title);
+          return (
+            <article className="grocery-item" key={id}>
+              <p className="title">{title}</p>
+              <div className="btn-container">
+                <button type="button" className="edit-btn"><FaEdit /></button>
+                <button type="button" className="delete-btn"><FaTrash /></button>
+              </div>
+            </article>
+          )
+        })
+      }
+    </div>
+  )
 }
 
 export default List
