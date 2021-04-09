1c7b7a03d0b4c3f46cc37c3ceea06dbefa0380df
mohdibrahimofficial <mohdibrahim972002@gmail.com>
2021-04-09 12:45:59 +0530

    Day 8 Navbar | Setup

--- 11-navbar/setup/src/App.js
+++ 11-navbar/setup/src/App.js
@@ -3,7 +3,7 @@ import Navbar from './Navbar'
 function App() {
   return (
     <>
-      <h2>navbar project setup</h2>
+      <Navbar />
     </>
   )
 }
--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -4,7 +4,61 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  const [showLinks,setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(()=>{
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if(showLinks)
+        linksContainerRef.current.style.height = `${linksHeight}px`;
+    else
+        linksContainerRef.current.style.height = "0px";
+
+  },[showLinks])
+
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo"/>
+          <button className="nav-toggle" onClick={()=>setShowLinks(!showLinks)}>
+            <FaBars />
+          </button>
+        </div>
+        
+            <div className='links-container' ref={linksContainerRef}>
+            <ul className="links" ref={linksRef}>
+                {
+                  links.map((link)=>{
+                      const {id,url,text} = link;
+                      return (
+                        <li>
+                          <a key={id} href={url}>{text}</a>
+                        </li>
+                      )
+                  })
+                }
+            </ul>
+          </div>
+        
+        <div className="social-icons">
+          <ul className="social-icons">
+              {
+                social.map((social)=>{
+                  const {id,url,icon} = social;
+                  return (
+                    <li>
+                      <a key={id} href={url}>{icon}</a>
+                    </li>
+                  )
+                })
+              }
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
